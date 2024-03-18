22aae7fb4a88ef4dfda3acfdb740a7ce8c64b0c5
Ryan Tse <ryantse100@gmail.com>
2024-03-18 11:02:42 -0700

    untested fn for bisim loss

---
+++ bc4rl.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from stable_baselines3.common.buffers import ReplayBuffer
+
+
+def bisim_loss(
+    replay_buffer: ReplayBuffer,
+    encoder: nn.Module,
+    critic: nn.Module,
+    c: float,
+    K: float,
+    n_samples: int,
+) -> torch.Tensor:
+    """
+    Estimates the difference between the L2 norm of the encoded space Z and the bisimulation
+    distance as approximated by the critic.
+
+    :param replay_buffer: (ReplayBuffer) the replay buffer
+    :param encoder: (nn.Module) the encoder
+    :param critic: (nn.Module) the critic
+    :param c: (float) the trade-off parameter between reward difference and transition difference
+    :param K: (float) the Lipschitz constant of the critic
+    :n_samples: (int) the number of samples to use for the estimation
+
+    :return: (torch.Tensor) the bisimulation loss
+    """
+    replay_samples = replay_buffer.sample(n_samples)
+
+    zs = encoder(replay_samples.observations)  # (n_samples, z_dim)
+    next_zs = encoder(replay_samples.next_observations)  # (n_samples, z_dim)
+    critique = critic(next_zs)  # (n_samples, 1)
+
+    zs_i = zs[:, None, :]
+    zs_j = zs[None, :, :]
+
+    critique_i = critique[:, None, :]
+    critique_j = critique[None, :, :]
+
+    rewards_i = replay_samples.rewards[:, None]
+    rewards_j = replay_samples.rewards[None, :]
+
+    encoded_distance = torch.norm(zs_i - zs_j, dim=2)  # TODO try L1
+
+    reward_distance = torch.abs(rewards_i - rewards_j)
+    critique_distance = torch.abs(critique_i - critique_j)  # TODO check abs?
+    bisim_distance = (1 - c) * reward_distance + c / K * critique_distance
+
+    return F.mse_loss(encoded_distance, bisim_distance)
