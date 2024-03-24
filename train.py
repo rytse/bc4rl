3b3ab2d0066a14e78d98aac4a22b71ba315c851a
Ryan Tse <ryantse100@gmail.com>
2024-03-23 22:08:14 -0700

    consolidate train.py

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import click
 import gymnasium as gym
 import imageio
 from stable_baselines3 import SAC
@@ -7,64 +8,113 @@ from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.sac.policies import MlpPolicy
 
+from bsac import BSAC, BisimConfig
 from encoder import CustomMLP
 
 LOG_DIR = Path("logs")
-LOG_DIR.mkdir(parents=True, exist_ok=True)
-
-VIDEO_DIR = Path("logs/videos")
-VIDEO_DIR.mkdir(parents=True, exist_ok=True)
 
 N_TRAIN_TIME_STEPS = 100_000
 N_CKPT_TIME_STEPS = N_TRAIN_TIME_STEPS // 10
 
-# Set up environment and callbacks
-env = gym.make("HalfCheetah-v4")
-eval_env = Monitor(gym.make("HalfCheetah-v4", render_mode="rgb_array"))
-checkpoint_callback = CheckpointCallback(
-    save_freq=N_CKPT_TIME_STEPS,
-    save_path=str(LOG_DIR),
-    name_prefix="rl_model",
-    save_replay_buffer=True,
-    save_vecnormalize=True,
-)
-eval_callback = EvalCallback(
-    eval_env,
-    best_model_save_path=str(LOG_DIR),
-    log_path=str(LOG_DIR),
-    eval_freq=N_CKPT_TIME_STEPS // 10,
-    deterministic=True,
-    render=True,
-)
+C = 0.5
+K = 1.0
+GRAD_PENALTY = 10.0
 
-# Train the agent
-model = SAC(
-    policy=MlpPolicy,
-    env=env,
-    policy_kwargs={
-        "features_extractor_class": CustomMLP,
-        "features_extractor_kwargs": {
-            "features_dim": 256,
-        },
-        "share_features_extractor": True,
-    },
-    device="cuda:1",
-)
-model.learn(
-    total_timesteps=N_TRAIN_TIME_STEPS,
-    callback=[checkpoint_callback, eval_callback],
-    progress_bar=True,
-)
+BISIM_BATCH_SIZE = 2048
+N_CRITIC_TRAINING_STEPS = 10
+N_ENCODER_TRAINING_STEPS = 10
 
-# Record a video of the trained agent
-obs, info = eval_env.reset()
-done = False
-frames = []
-while not done:
-    action, _ = model.predict(obs, deterministic=True)
-    obs, reward, terminated, truncated, _ = eval_env.step(action)
-    done = terminated or truncated
-    frames.append(eval_env.render())
-eval_env.close()  # https://github.com/google-deepmind/mujoco/issues/1186
 
-imageio.mimsave(VIDEO_DIR / "trained_agent.mp4", frames, fps=20)
+@click.command()
+@click.argument("algo", type=click.Choice(["sac", "bsac"]), help="RL algo to use")
+@click.argument("env_id", type=str, default="HalfCheetah-v4", required=False)
+@click.argument("device", type=str, default="cuda:0", required=False)
+def main(algo: str, env_id: str, device: str):
+    log_dir = LOG_DIR / f"{algo}_{env_id}"
+    tb_dir = log_dir / "tensorboard"
+    video_dir = log_dir / "videos"
+    log_dir.mkdir(parents=True, exist_ok=True)
+    tb_dir.mkdir(parents=True, exist_ok=True)
+    video_dir.mkdir(parents=True, exist_ok=True)
+
+    env = gym.make(env_id)
+    eval_env = Monitor(gym.make(env_id, render_mode="rgb_array"))
+
+    ckpt_cb = CheckpointCallback(
+        save_freq=N_CKPT_TIME_STEPS,
+        save_path=str(log_dir),
+        name_prefix="rl_model",
+        save_replay_buffer=True,
+        save_vecnormalize=True,
+    )
+    eval_cb = EvalCallback(
+        eval_env,
+        best_model_save_path=str(log_dir),
+        log_path=str(log_dir),
+        eval_freq=N_CKPT_TIME_STEPS // 10,
+        deterministic=True,
+        render=True,
+    )
+
+    if algo == "sac":
+        model = SAC(
+            policy=MlpPolicy,
+            env=env,
+            policy_kwargs={
+                "features_extractor_class": CustomMLP,
+                "features_extractor_kwargs": {
+                    "features_dim": 256,
+                },
+                "share_features_extractor": True,
+            },
+            device=device,
+            tensorboard_log=str(tb_dir),
+        )
+    elif algo == "bsac":
+        model = BSAC(
+            policy=MlpPolicy,
+            env=env,
+            bisim_config=BisimConfig(
+                C=C,
+                K=K,
+                grad_penalty=GRAD_PENALTY,
+                batch_size=BISIM_BATCH_SIZE,
+                critic_training_steps=N_CRITIC_TRAINING_STEPS,
+                encoder_training_steps=N_ENCODER_TRAINING_STEPS,
+            ),
+            policy_kwargs={
+                "features_extractor_class": CustomMLP,
+                "features_extractor_kwargs": {
+                    "features_dim": 256,
+                },
+                "share_features_extractor": True,
+            },
+            device="cuda:1",
+            tensorboard_log=str(tb_dir),
+        )
+    else:
+        raise ValueError(f"Invalid algo: {algo}")
+
+    # Train
+    model.learn(
+        total_timesteps=N_TRAIN_TIME_STEPS,
+        callback=[ckpt_cb, eval_cb],
+        progress_bar=True,
+    )
+
+    # Eval, record
+    obs, _ = eval_env.reset()
+    done = False
+    frames = []
+    while not done:
+        action, _ = model.predict(obs, deterministic=True)
+        obs, _, terminated, truncated, _ = eval_env.step(action)
+        done = terminated or truncated
+        frames.append(eval_env.render())
+    eval_env.close()  # https://github.com/google-deepmind/mujoco/issues/1186
+
+    imageio.mimsave(video_dir / "trained_agent.mp4", frames, fps=20)
+
+
+if __name__ == "__main__":
+    main()
