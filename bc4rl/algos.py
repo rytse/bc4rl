479da0fa572628a0bdb6306d18beb6374b3f0371
Ryan Tse <ryantse100@gmail.com>
2024-03-30 17:10:38 -0700

    tb log encoder loss

--- bc4rl/algos.py
+++ bc4rl/algos.py
@@ -286,6 +286,7 @@ class BSAC(SAC):
         self.logger.record("train/ent_coef", np.mean(ent_coefs))
         self.logger.record("train/actor_loss", np.mean(actor_losses))
         self.logger.record("train/critic_loss", np.mean(critic_losses))
+        self.logger.record("train/encoder_loss", np.mean(encoder_losses))
         self.logger.record("train/bisim_critic_loss", np.mean(bs_critic_losses))
         if len(ent_coef_losses) > 0:
             self.logger.record("train/ent_coef_loss", np.mean(ent_coef_losses))
