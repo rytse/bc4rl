9a4b6c501ed74b20b16e7e1d6f966bee2a00c5b0
Ryan Tse <ryantse100@gmail.com>
2024-04-03 12:00:08 -0700

    test working SAC path with eval.py (needs update, very adhoc)

--- eval.py
+++ eval.py
@@ -8,26 +8,26 @@ from stable_baselines3.common.monitor import Monitor
 
 from experiments.envs import get_env
 
-LOG_DIR = Path("logs/sac_CnnPolicy_carracer")
+LOG_DIR = Path("logs/old/experiment_manager_test/sac/LunarLanderContinuous-v2_4")
 VIDEO_DIR = LOG_DIR / "videos"
+VIDEO_DIR.mkdir(exist_ok=True)
 
-train_env, eval_env = get_env("carracer")
+train_env, eval_env = get_env("lunarlander", 1)
 # env = gym.make("HalfCheetah-v4")
 # eval_env = Monitor(gym.make("HalfCheetah-v4", render_mode="rgb_array"))
 
 model_path = LOG_DIR / "best_model.zip"
-model = SAC.load(model_path, env=eval_env, device="cuda:1")
+model = SAC.load(model_path, env=eval_env, device="cuda:0")
 
-obs, info = eval_env.reset()
+obs = eval_env.reset()
 done = False
 frames = []
 while not done:
     obs = np.array(obs)
     action, _ = model.predict(obs, deterministic=True)
-    obs, reward, terminated, truncated, _ = eval_env.step(action)
-    done = terminated or truncated
+    obs, reward, dones, _ = eval_env.step(action)
+    done = dones.any()
 
-    breakpoint()
     frames.append(eval_env.render())
 eval_env.close()  # https://github.com/google-deepmind/mujoco/issues/1186
 
