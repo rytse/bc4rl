7bd707940bfc835006ba261ecb067887e771dd41
Ryan Tse <ryantse100@gmail.com>
2024-09-25 23:44:42 -0400

    BPPO_4

--- hyperparams/bppo.py
+++ hyperparams/bppo.py
@@ -31,7 +31,7 @@ hyperparams = {
         "n_epochs": 4,
         "n_steps": 512,
         "n_timesteps": 10_000_000,
-        "bisim_weight": 0.0,
+        "bisim_weight": 0.005,
         "bisim_c": 0.75,
         "bisim_stop_gradient": True,
         "bisim_tau": 0.005,
